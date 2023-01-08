0eacc715a8c9303f973dc493704313b41e6d8509
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2023-01-08 12:23:50 +0100

    added brands

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google'
+import slack from '../../assets/slack'
+import atlassian from '../../assets/atlassian'
+import dropbox from '../../assets/dropbox'
+import shopify from '../../assets/shopify'
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
